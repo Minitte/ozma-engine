979190f8ae7387a6e25401d0a30dd42a11d14570
Davis Pham <arcentalminitte@gmail.com>
2018-04-04 14:17:47 -0700

    more vector2 operations

--- src/application/math/Vector2.java
+++ src/application/math/Vector2.java
@@ -21,7 +21,51 @@ public class Vector2 {
 		this.x = x;
 		this.y = y;
 	}
+	
+	/**
+	 * Dot product operation between this and the other vector
+	 * @param other
+	 * @return
+	 */
+	public float dot(Vector2 other) {
+		return (x * other.x) + (y * other.y);
+	}
 
+	/**
+	 * Adds another vector to this vector
+	 * @param other
+	 */
+	public void add(Vector2 other) {
+		x += other.x;
+		y += other.y;
+	}
+	
+	/**
+	 * multiplies both x and y with the given scale factor
+	 * @param scale
+	 * @return
+	 */
+	public void linearMutliply(float scale) {
+		x *= scale;
+		y *= scale;
+	}
+	
+	/**
+	 * multiplies both x and y with the x and y in other
+	 * @param other
+	 */
+	public void multiply(Vector2 other) {
+		x *= other.x;
+		y *= other.y;
+	}
+	
+	/**
+	 * Creates a new vector2 with the same values
+	 */
+	public Vector2 clone() {
+		return new Vector2(x, y);
+	}
+	
 	/**
 	 * @return the x
 	 */
