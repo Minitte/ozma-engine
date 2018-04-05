ff6c1a27e585b7e6331f14b1d8e9960a48aa75fe
Minitte <arcentalminitte@gmail.com>
2018-04-04 22:44:57 -0700

    divide operator

--- src/application/math/Vector2.java
+++ src/application/math/Vector2.java
@@ -69,10 +69,44 @@ public class Vector2 {
 	}
 	
 	/**
+	 * divdes both x and y by the given denominator
+	 * @param denominator
+	 */
+	public void linearDivide(float denominator) {
+		x /= denominator;
+		y /= denominator;
+	}
+	
+	/**
+	 * divides both x and y by the x and y in other
+	 * @param other
+	 */
+	public void divide(Vector2 other) {
+		x /= other.x;
+		y /= other.y;
+	}
+	
+	/**
 	 * Creates a new vector2 with the same values
 	 */
 	public Vector2 clone() {
 		return new Vector2(x, y);
+	}
+	
+	/**
+	 * Creates a new vector representing the normal
+	 * @return
+	 */
+	public Vector2 getNormal() {
+		return new Vector2(-y, x);
+	}
+	
+	/**
+	 * the length of the vector by calculating the hypotenuse
+	 * @return
+	 */
+	public float getLength() {
+		return (float)Math.sqrt((x * x) + (y * y));
 	}
 	
 	/**
